35cea254ccadae8fa3c6a3044c120a26b84982a6
时点软件冯成林 <nsdictionary@icloud.com>
2017-07-24 11:02:14 +0800

    update

--- pages/FrameWorks/CoreNewFeature/CoreNewFeature.js
+++ pages/FrameWorks/CoreNewFeature/CoreNewFeature.js
@@ -1,3 +1,51 @@
 function CoreNewFeature(){}
 
+CoreNewFeature.anim_creator = wx.createAnimation({
+
+  transformOrigin: "50% 50% 0",
+  duration: 1200,
+  timingFunction: "ease-out",
+  delay: 0
+
+});
+
+CoreNewFeature.showInVC = function(vc, version, imgs){
+  let key = 'CoreNF_version'
+  let lastVersion = wx.getStorageSync(key)
+
+  if (typeof lastVersion == 'undefined' || version <= lastVersion) {return}
+
+  wx.setStorage({
+    key: key,
+    data: version
+  })
+
+  let count = imgs.length
+
+  vc.CoreNF_changeAction = function (e) {
+
+    let CoreNF_now_page = e.detail.current 
+    vc.CoreNF_now_page = CoreNF_now_page
+  }
+
+  vc.CoreNF_tapAction = function () {
+
+    let current_page = typeof vc.CoreNF_now_page == 'undefined' ? 0 : vc.CoreNF_now_page
+
+    if (current_page != count-1) {return}
+
+    //CoreNF_anim
+    CoreNewFeature.anim_creator.opacity(0).translateX(-500).step()
+
+    this.setData({ CoreNF_anim: CoreNewFeature.anim_creator.export(), CoreNF_Ready:true})
+    setTimeout(() => {
+      this.setData({ CoreNF_hide: true, CoreNF_Ready: false})
+    }, 1300)
+  }
+
+  vc.setData({ CoreNF_imgs: imgs, CoreNF_Ready: true})
+
+
+}
+
 module.exports = CoreNewFeature
--- pages/Index/Index.js
+++ pages/Index/Index.js
@@ -14,7 +14,18 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
+
   },
+
+  onShow: function(){
+    let imgs = [
+      "http://im6.leaderhero.com/wallpaper/213/9387294b039c442f9f23470caf8dc238.jpg",
+      "http://im6.leaderhero.com/wallpaper/213/9387294b039c442f9f23470caf8dc238.jpg",
+      "http://im6.leaderhero.com/wallpaper/213/9387294b039c442f9f23470caf8dc238.jpg",
+      "http://im6.leaderhero.com/wallpaper/213/9387294b039c442f9f23470caf8dc238.jpg"
+    ]
+    CoreNewFeature.showInVC(this, '1.5', imgs)
+  }
 
 })
